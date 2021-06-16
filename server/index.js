201bc0e625efcf8b1e8e65876945a092003345eb
Ujjwal <ujjwal0035@gmail.com>
2021-06-16 23:04:29 +0530

    second commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express =require('express');
+const app=express();
+const mongoose = require("mongoose");
+const bodyParser=require("body-parser")
+const dotenv = require("dotenv");
+
+const authRoute = require("./route/auth");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("SuccessFully Connected to MongoDB");
+  }
+);
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/api/auth",authRoute);
+
+app.listen(8800,()=>{
+    console.log("app is running on 8800 port")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//cloud mongodb
+//mongodb+srv://SocialApp:Ujjwal@123@ujjuapp.k9hdn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
+//local mongodb
+//mongodb://practice:123@localhost:27017/practiceApp
